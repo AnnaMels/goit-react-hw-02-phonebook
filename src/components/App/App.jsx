7da75450f072e6d826bbc97b2dfc725bd2c8f379
Anna Melnichuk <varchuk.anna.94@gmail.com>
2022-08-02 11:02:54 +0300

    Edited homepage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,13 @@
 import { Component } from 'react';
-import Form from './components/ContactForm';
-import ContactList from './components/ContactsList';
-import Filter from './components/Filter';
-import { PhonebookTitle } from './components/App/App.styles';
-import { ContactsTitle } from './components/App/App.styles';
+import Form from '../ContactForm';
+import ContactList from '../ContactsList';
+import Filter from '../Filter';
+import { PhonebookTitle } from './App.styles';
+import { ContactsTitle } from './App.styles';
 
 export class App extends Component {
       state = {
-        contacts: [    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+        contacts: [],
         filter: ''
       }
   
